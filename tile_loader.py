60e175b49185372b2d48a9d039decfbd0b6a415d
Bastian Neumann <bneumann@kurzschluss-engineering.de>
2021-08-26 20:04:50 +0200

    changes for v1

--- tile_loader.py
+++ tile_loader.py
@@ -1,3 +1,4 @@
+from os import write
 import gpxpy
 import gpxpy.gpx
 import download
@@ -12,6 +13,8 @@ gpx = gpxpy.parse(gpx_data)
 lat=[90,-90]
 lon=[180,-180]
 
+t = open("TRACK", "w")
+
 for track in gpx.tracks:
     for segment in track.segments:
         for point in segment.points:
@@ -19,6 +22,8 @@ for track in gpx.tracks:
             if(point.latitude > lat[1]): lat[1] = point.latitude
             if(point.longitude < lon[0]): lon[0] = point.longitude
             if(point.longitude > lon[1]): lon[1] = point.longitude
+            t.write("{} {}\n".format(point.longitude, point.latitude))
+
             
 print('Box at ({0},{1}) -> ({2},{3})'.format(lat[0], lon[0], lat[1], lon[1]))
-download.get_map(lon, lat, [16], jobfolder)
+download.get_map(lon, lat, [16,13], jobfolder)
